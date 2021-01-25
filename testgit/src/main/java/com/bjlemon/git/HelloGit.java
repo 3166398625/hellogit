739a949d769c1b31193bf6c7f2ee62630bbc1c0f
3166398625 <77004314+3166398625@users.noreply.github.com>
2021-01-26 01:00:27 +0800

    Update HelloGit.java

--- testgit/src/main/java/com/bjlemon/git/HelloGit.java
+++ testgit/src/main/java/com/bjlemon/git/HelloGit.java
@@ -9,5 +9,6 @@ public class HelloGit {
         System.out.println("创建分支");
         System.out.println("master add");
         System.out.println("other branch add ");
+        System.out.println("online edit ");
     }
 }
